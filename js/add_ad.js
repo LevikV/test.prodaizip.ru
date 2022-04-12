a206305aeb0509ccec5549d05e3896b4a091be9b
LevikV <alex@kplus79.ru>
2022-04-12 12:49:28 +1000

    Работаю над Безопасной сделкой в добавлении объявления

--- js/add_ad.js
+++ js/add_ad.js
@@ -1,3 +1,14 @@
 $('.upload-new>.row').on('click', function() {
     $('#image-add').trigger('click');
 });
+
+$('#select-type-ad').change(function() {
+    var val = $('#select-type-ad').val();
+    if (val == 'Продать') {
+        $('.safe-trade').css('display', 'flex');
+    } else {
+        $('.safe-trade').css('display', 'none');
+    }
+
+
+});
